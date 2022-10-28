ed99121730c8d9e69fffdb67e061dce545352c63
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-27 20:46:53 -0500

    update

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var width=0;
+var height=0;
+
+function game(width, height){
+    this.width=width;
+    run();
+}
+
+function run(){
+    let running=true;
+    while(running){
+        running=false;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,9 @@
+import game from game;
+
+function launcher(){
+    var width = 480;
+    var height = 360;
+    game(width,height);
+}
+
+launcher;
